c8a29db24af06162fa38cbff1acc0adf4172253e
jmhar <jmhar@protonmail.com>
2021-09-09 16:34:22 +1000

    commit tree 42d92590f531516d41ed9e3322ee3edecc28495d

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize sender, receiver, amount
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = 'pending'
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && @status == 'pending' && @receiver.balance > amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @status = "reversed"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+    end
+  end
 end
